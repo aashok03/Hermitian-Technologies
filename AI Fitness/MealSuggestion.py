dda95fb5db3d1bfef57189d4c1e46c6837d3db14
aryamanpandya <66801737+aryamanpandya@users.noreply.github.com>
2020-06-13 15:38:52 -0400

    Add files via upload

---
+++ AI Fitness/MealSuggestion.py
@@ -0,0 +1,29 @@
+import mysql.connector as mysql
+
+
+#Calculates caloric and macronutrient intake levels based on user information
+#and dependent on the user's goals 
+def suggestedIntake(weight, BMR, goal, ECB):
+	caloricOutput = BMR+ECB
+	if goal == 0:
+		caloricIntake = 0.85*caloricOutput
+	elif goal == 1:	
+		caloricIntake = caloricOutput
+    elif goal == 2:
+    	caloricIntake = 1.15*caloricOutput
+    proteinCalories = 0.18*caloricIntake 
+    protein = proteinCalories/4
+    carbCalories = 0.55*caloricIntake
+    carbs = carbCalories/4
+    fatCalories = 0.27*caloricIntake
+    fat = fatCalories/5
+    optimalLevels = [caloricIntake, protein, carbs, fat] #List of intake data
+    #calories followed by macronutrients in grams 
+    return optimalLevels
+
+#uses values calculated from above function + allergy data 
+#to make meal recommendations to the user
+def mealSuggestion(suggestedLevels, allergies):
+	
+
+
